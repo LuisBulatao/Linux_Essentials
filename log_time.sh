c9c2cc7e808e44cd8512c3a15c168ea533e49504
Luis Joshua Bulatao <bluisjoshua@gmail.com>
2025-04-07 13:55:56 +0000

    Activity 6 For OS

--- log_time.sh
+++ log_time.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 echo "Current Date and Time: $(date)" >> log.txt
 
+github_username=$GITHUB_ACTOR
 
+if [ ! -f log.txt ]; then
+    echo "Log File Created at $current_time by $github_username" > log.txt
+
+else   
+    update_count=$(wc -1 < log.txt)
+    echo "Log update #Supdate_count: $current_time by $github_username" >> log.txt
+fi
